da483720cfeabeaa9484e34249d89baea60216b8
Fisnik Kurti <47564531+fisnikkk@users.noreply.github.com>
2023-03-09 10:50:49 +0100

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from .models import Product
 
+def index(request):
+    message = "Welcome to my app"
+    return render(request, 'index.html', {'message': message})
+
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'product_list.html', {'products': products})
+
+
+def about(request):
+    return render(request, 'about.html')
