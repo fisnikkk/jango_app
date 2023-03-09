c2c421497786ddff024470a762c77dbc7d9d6e22
Fisnik Kurti <47564531+fisnikkk@users.noreply.github.com>
2023-03-09 10:50:47 +0100

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('products/', views.product_list, name='product_list'),
+    path('about/', views.about, name='about'),
 ]
